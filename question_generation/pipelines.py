33f7aac01a9a07b04e404b1029fe346c86a56e21
Zheyu Chen <zheyu.chen@ucdconnect.ie>
2022-04-27 18:07:46 +0800

    kk

--- question_generation/pipelines.py
+++ question_generation/pipelines.py
@@ -69,7 +69,7 @@ class QGPipeline:
         title = "index" + "\t" + "label" + "\t" + "answer" + "\t" + "question" "\n"
         file.write(title)
         for index, output_dict in enumerate(output):
-            write_in: str = index + "\t" + output_dict['answer'] + '\t' + output_dict['question'] + '\n'
+            write_in: str = str(index) + "\t" + output_dict['answer'] + '\t' + output_dict['question'] + '\n'
             file.write(write_in)
         file.close()
 
