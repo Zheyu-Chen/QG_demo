4d937ff65f007fe4e6eab274c8de4a965631e73e
Zheyu Chen <zheyu.chen@ucdconnect.ie>
2022-04-28 22:03:55 +0800

    update

--- question_generation/pipelines.py
+++ question_generation/pipelines.py
@@ -97,20 +97,21 @@ class QGPipeline:
 
     def _extract_answers(self, context):
         sents, inputs = self._prepare_inputs_for_ans_extraction(context)
+        print("inputs: " + str(inputs))
         inputs = self._tokenize(inputs, padding=False, truncation=True)
-        # print("sents: " + str(sents))
-        # print("inputs: " + str(inputs))
+        print("sents: " + str(sents))
+        print("inputs: " + str(inputs))
 
         outs = self.ans_model.generate(
             input_ids=inputs['input_ids'].to(self.device),
             attention_mask=inputs['attention_mask'].to(self.device),
             max_length=32,
         )
-        print("outs: " + str(outs))
+        # print("outs: " + str(outs))
         dec = [self.ans_tokenizer.decode(ids, skip_special_tokens=False) for ids in outs]
         answers = [item.split('<sep>') for item in dec]
-        print("dec: " + str(dec))
-        print("answers: " + str(answers))
+        # print("dec: " + str(dec))
+        # print("answers: " + str(answers))
         answers = [i[:-1] for i in answers]
 
         return sents, answers
