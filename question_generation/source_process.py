ef9afe0f1f942301fb22fb8d09ebb8240f9d47a8
Zheyu Chen <zheyu.chen@ucdconnect.ie>
2022-04-29 21:57:15 +0800

    update

--- question_generation/source_process.py
+++ question_generation/source_process.py
@@ -4,19 +4,17 @@ with open('./source/prevention_and_control.txt', 'r', encoding="utf8") as file:
     count_file = 0
     count_line = 0
     for line in file:
-        if count_file <= 10:
-            line = line.strip()
-            if len(line) == 0:
-                continue
-            if count_line <= 10:
-                file_new.write(line + '\n')
-                count_line += 1
-                print(count_line)
-            else:
-                file_new.close()
-                count_file += 1
-                count_line = 0
-                file_new = open('./process/prevention_and_control_' + str(count_file) + '.txt', 'a',
-                                encoding="utf8")
-                file_new.write(line + '\n')
-
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        if count_line <= 20:
+            file_new.write(line + '\n')
+            count_line += 1
+            print(count_line)
+        else:
+            file_new.close()
+            count_file += 1
+            count_line = 0
+            file_new = open('./process/prevention_and_control_' + str(count_file) + '.txt', 'a',
+                            encoding="utf8")
+            file_new.write(line + '\n')
