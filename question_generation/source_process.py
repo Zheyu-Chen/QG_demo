ff1906b47ff5a679f00dc0eb5692b6a46c4fb12d
Zheyu Chen <zheyu.chen@ucdconnect.ie>
2022-05-16 19:06:54 +0800

    update

--- question_generation/source_process.py
+++ question_generation/source_process.py
@@ -8,7 +8,7 @@ def pre_process(source_file):
             line = line.strip()
             if len(line) == 0:
                 continue
-            if count_line < 10:
+            if count_line < 0:
                 file_new.write(line + '\n')
                 count_line += 1
                 print(count_line)
@@ -22,8 +22,9 @@ def pre_process(source_file):
 
 
 if __name__ == '__main__':
-    pre_process('0_prevention_and_control')
-    pre_process('1_health_and_medicine')
-    pre_process('2_event_and_timeline')
-    pre_process('3_character')
-    pre_process('4_epidemic')
+    # pre_process('0_prevention_and_control')
+    # pre_process('1_health_and_medicine')
+    # pre_process('2_event_and_timeline')
+    # pre_process('3_character')
+    # pre_process('4_epidemic')
+    pre_process('x_others')
